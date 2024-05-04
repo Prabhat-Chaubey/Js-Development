8e5c243c4d8d690e25e14e8ee502083eff84fb3d
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-05-04 15:13:20 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const AccountId = 1223344
+let AccountEmail = "prabhatbhai@gmail.com"
+var AccountPassword = "12345"
+Accountcity = "jaipur" 
+// aise bhi decalre kar sakte but kara  nahi chahiye
+
+// AccountId = 23 // Not allowed
+
+console.log(AccountId
+);
+
+// hume var use karne hi kuch jarurat nahi hai sirf let and const pe hi kaam chalege humesha, const baad me change nahi ho sakta, but let change ho jaenga
+
+console.table([AccountId,AccountEmail,AccountPassword,Accountcity]);
