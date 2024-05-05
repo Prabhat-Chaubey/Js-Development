039adb7525db02c40f2df3c36c43b3b53dc59b25
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-05-05 06:47:26 +0000

    commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+// alert (3+3)
+
+// this fcntion when integrated with the html and engines then it works and if we want to se is directly sing node then its not possible
+
+console.log(3+3) 
+console.log("hello")
+
+//code readiblity shold be high
+//for docs always use mdn docs for example alert mdn
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
