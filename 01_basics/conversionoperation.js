77c215d94cc31a9d762e61cf28c386753e940851
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-05-05 08:57:31 +0000

    commit

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -22,3 +22,66 @@ if isLoggedIn = "" => false
 if isLoggedIn = "stringnot empty" => true
 
 */
+
+// ============operations===============
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+/*
+
+console.log("addition operation: ",2+2);
+console.log("subtraction operation: ",2-2);
+console.log("multiplication operation: ",2*2);
+console.log("poweer operation: ",2**3);
+console.log("division operation: ",2/2);
+console.log("remainder operation: ",2%2);
+
+*/
+
+// we can add/concatinate string as well
+
+let str1 = "hello"
+let str2 = " prabhat"
+console.log(str1+str2)
+console.log(1 + str2)
+
+console.log("1"+1+1)
+
+// starting with string hence everything is treated as string 
+
+console.log(1+1+"1")
+
+// once starting is int everything is in nnumber
+
+console.log("1"+1+"1")
+
+// this would alse play as string 
+
+
+let gamecounter = 100
+gamecounter ++
+
+// postfix increment
+
+console.log(gamecounter)
+-- gamecounter
+
+// prefix increment 
+
+
+console.log(gamecounter)
+
+
+let x= 3
+
+let y = x++
+console.log("postfix incrent of 3 is ",y)
+// this would give output as y=3, as action would happen after it, that means in another run or cycle the increment value would be appended
+
+let a= 3
+
+let b = ++a
+console.log("prefix incrent of 3 is ",b)
+// this would give output as b=4, as action would happen before it, that means in the curent    cycle the increment value would be appended
