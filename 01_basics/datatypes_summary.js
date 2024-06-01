cb0dccc36f3719b73c47cb1ec78adf6f34f09853
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-06-01 15:25:03 +0000

    commit

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,20 @@
+data is categorized into two type 
+primitive/value type
+there are 7 types of primitve data type 
+String 
+Number 
+Bolean 
+null
+undefined
+Symbol
+BigInt
+
+
+non primitive/reference type 
+
+array 
+objects 
+functons
+
+
+// javascript is a dynamicaaly typed language, because if we do (const = 100) then that data type of const would be int, but we have not mentoned it to be int or String, hnce iit is a dynamic langugae 
