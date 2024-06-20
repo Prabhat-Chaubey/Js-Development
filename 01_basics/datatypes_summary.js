f3a7ed68d1fa83531feaa2b13912b001d854df4c
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-06-20 15:16:07 +0000

    Commit

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -46,3 +46,74 @@ myFunction();
 console.log(typeof myFunction)
 console.log(typeof SuperHeroes)
 console.log(typeof student)
+
+
+
+
+// stack and heap memory 
+
+// there are two storage ways, stack and heap memory type 
+
+// Stack Memory (In primitive Types)
+// Heap Memory (In Non-Primitive Types)
+
+// in stack memory we get the copy and not the original one 
+
+let Firstname = "Prabhat";
+let OneMoreName = Firstname;
+
+console.log(Firstname);
+console.log(OneMoreName);
+
+// till now one more name have value as "Prabhat"
+
+OneMoreName = "Ruchi"
+
+/* Now
+ Firstname -> Prabhat
+ OneMoreName -> Ruchi
+ Because we made changes in the copy 
+*/
+
+// In HeapMemory we get the references , which means 
+
+let UserOne = {
+    email : "abc@gmail.com",
+    upi : "abc123"
+}
+
+console.log(UserOne);
+
+/*         _________________________________
+UserOne -> |    {                           |
+           |    email : "abc@gmail.com",    |
+           |    upi : "abc123"              |
+           |        }                       |
+           _________________________________
+                        Heap Memory
+
+           Now its visible that the UserOne is having the refference on one heap memory
+*/
+
+let UserTwo = UserOne
+console.log(UserTwo);
+
+/*         _________________________________
+UserOne -> |    {                           |
+UserTwo -> |    email : "abc@gmail.com",    |
+           |    upi : "abc123"              |
+           |        }                       |
+           _________________________________
+                        Heap Memory
+
+           Now its visible that the UserOne and userTwo both having the refference on one heap memory
+*/
+
+UserTwo.email="Ruchi@gmail.com"
+
+/*
+Now SInce we have the refernce of same heap memory so it would update accordingly
+*/
+
+console.log(UserOne.email);
+console.log(UserTwo.email);
