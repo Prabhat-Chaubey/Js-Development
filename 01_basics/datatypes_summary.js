f84c99a5199b087d833c66f8d6e8ae694067779b
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-06-20 06:02:21 +0000

    commit

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,20 +1,48 @@
-data is categorized into two type 
-primitive/value type
-there are 7 types of primitve data type 
-String 
-Number 
-Bolean 
-null
-undefined
-Symbol
-BigInt
+// data is categorized into two type 
+// primitive/value type
+// there are 7 types of primitve data type 
+// String 
+// Number 
+// Bolean 
+// null
+// undefined
+// Symbol
+// BigInt
 
 
-non primitive/reference type 
+// non primitive/reference type 
 
-array 
-objects 
-functons
+// Array
 
+const SuperHeroes = ["shaktiman","hulk","Thor"]
+console.log(SuperHeroes)
+
+// objects
+
+let student = {
+    name : "prabhat",
+    age : 12,
+    division : "c",
+    rollcall : 77
+}
+console.log(student)
+console.log(student.name)
+
+
+
+// functons
+
+
+const myFunction = function(){
+    console.log("hello fucntion")
+}
+myFunction();
 
 // javascript is a dynamicaaly typed language, because if we do (const = 100) then that data type of const would be int, but we have not mentoned it to be int or String, hnce iit is a dynamic langugae 
+
+
+// we cant go back and check the data type again and again so we have a fucntion *typeof* which helps to know the type of the data we are using 
+
+console.log(typeof myFunction)
+console.log(typeof SuperHeroes)
+console.log(typeof student)
