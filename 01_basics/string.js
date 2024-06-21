6238f1cdea58745a3e186d3a29143694e00af5ea
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-06-21 12:13:47 +0000

    Commit

---
+++ 01_basics/string.js
@@ -0,0 +1,15 @@
+const Myname = "prabhat"
+const Repocount = 10
+
+console.log(Myname + Repocount + " halaluya");
+
+/*but this is very old way to write it so we would be using the string polarization */
+
+console.log(`my name is ${Myname} and my repocount is ${Repocount}`);
+
+/*
+output
+my name is prabhat and my repocount is 10
+
+
+there are more of the methods we need to know and understand , and would get to know about it once we dig depper*/
