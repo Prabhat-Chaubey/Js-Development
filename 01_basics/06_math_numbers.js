eeda1b2a4b454e21d1d2e18908cfab0060782a2e
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-06-21 16:08:31 +0000

    Maths

---
+++ 01_basics/06_math_numbers.js
@@ -0,0 +1,17 @@
+const Score = 400
+console.log(Score);
+console.log(typeof(Score));
+console.log(Score.toString().length);
+console.log(Score.toFixed(2));
+
+const OtherNumber = 123.9
+console.log(OtherNumber.toPrecision(3));
+
+const Hundereds = 10000000
+console.log(Hundereds.toLocaleString('en-In'));
+
+console.log(Math.ceil(4.1));
+
+console.log(Math.max(2,5,1,4,7));
+
+console.log((Math.round(Math.random()) * 10 + 1 ));
