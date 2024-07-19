c63527615c12248e6cc701deb8c0487ea23a8d5e
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-07-19 06:35:24 +0000

    commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,4 +19,50 @@ function AddTwoNumbers (number1,number2){
 }
 
 const Final = AddTwoNumbers(3,5)
-console.log(Final);
+// console.log(Final);
+
+//rest operations
+
+// we have a lot of entries we are unaware of then we do 
+
+function CartValue(...num1){
+    return num1
+}
+
+console.log(CartValue(100,500,200));
+
+//  [ 100, 500, 200 ]
+
+// handling an object
+// handling an object in fucntion 
+
+// creating an Object 
+
+uerValue = {
+    UserName : "Prabhat",
+    phoneNo  : 7770098538
+
+}
+
+// now creating functions to handle the object 
+
+function HandleObject(AnyObject){
+    console.log(`the name of User is ${AnyObject.UserName} and phone number is ${AnyObject.phoneNo}`);
+}
+HandleObject(uerValue)
+
+// the name of User is Prabhat and phone number is 7770098538
+
+
+// passing array as argument
+
+
+const MyArray = [100,200,400,100,600]
+
+function HandleArray (NewArray){
+    console.log(NewArray[1])
+}
+
+HandleArray(MyArray)
+
+// 200
