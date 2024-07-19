946eae6efcfb23c1886786df1233689de70229eb
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2024-07-19 05:49:20 +0000

    commit

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+// How to declare a functions 
+
+function SayMyName (){
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("T");
+
+}
+
+// SayMyName()
+
+function AddTwoNumbers (number1,number2){
+    let result = number1+number2
+    return result
+}
+
+const Final = AddTwoNumbers(3,5)
+console.log(Final);
